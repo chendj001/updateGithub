8213de2e0fb32733126ce28daffbf4ce7971ed02
九转灵山 <chendj89@hotmail.com>
2023-11-14 20:13:29 +0800

    3232

--- src/utils/__tests__/index.spec.ts
+++ src/utils/__tests__/index.spec.ts
@@ -3,8 +3,8 @@ import { encrypt, decrypt } from '../crypto'
 import { sleep } from '../sleep'
 
 describe('加密和解密', () => {
-  let str = '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'
-  let key = '896351'
+  let str = '123'
+  let key = '321'
   it('加密', () => {
     let result = encrypt(str, key)
     console.log(result)
